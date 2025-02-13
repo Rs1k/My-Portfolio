0371978b8dc535a3d1d67c6928a0f25b06966886
Rs1k <rostiksmoliar@gmail.com>
2025-02-13 14:29:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => 
-    console.log(`Сервер запущен на порту ${PORT}`)
-);
-
 app.use(express.json());
 app.use(cors());
 
